1b0b72a15517a2e7ebc2df3d61bc2f46da2e71e8
ponsAoki <aokishogo20pk@gmail.com>
2022-06-17 13:55:57 +0900

    ex.1 done

--- ex1. 円の面積/circle_area.py
+++ ex1. 円の面積/circle_area.py
@@ -3,9 +3,24 @@ import math
 
 args = sys.argv
 
-print(args[1])
-r = float(args[1])
+# print(args[1])
 
-area = math.pi * r * r
+def is_num(s):
+  try:
+      float(s)
+  except ValueError:
+      return False
+  else:
+      return True
 
-print('{0}'.format(area))
+if len(args) <= 1:
+  print("入力値がありません")
+elif len(args) > 1 and not is_num(args[1]): 
+  print("数値を入力してください")
+elif is_num(args[1]): 
+  r = float(args[1])
+  area = math.pi * r * r
+
+  print('{0}'.format(area))
+
+
